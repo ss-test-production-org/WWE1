b0407e6b9fe05c6bb0c0fd635cb2fa38dc429307
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2023-02-23 10:02:31 +0800

    DEV: Port sidebar mobile view acceptance tests to system tests
    The acceptance tests are flaky so I've decided to port them to system
    test which makes them easier to work with and reason about.

--- spec/system/page_objects/components/sidebar_header_dropdown.rb
+++ spec/system/page_objects/components/sidebar_header_dropdown.rb
@@ -7,8 +7,43 @@ module PageObjects
         page.find(".hamburger-dropdown").click
       end
 
+      SIDEBAR_HAMBURGER_DROPDOWN = ".sidebar-hamburger-dropdown"
+
+      def width
+        page.find(SIDEBAR_HAMBURGER_DROPDOWN).rect.width
+      end
+
       def visible?
-        page.has_css?(".sidebar-hamburger-dropdown")
+        page.has_css?(SIDEBAR_HAMBURGER_DROPDOWN)
+      end
+
+      def hidden?
+        page.has_no_css?(SIDEBAR_HAMBURGER_DROPDOWN)
+      end
+
+      def has_no_keyboard_shortcuts_button?
+        page.has_no_css?(".sidebar-footer-actions-keyboard-shortcuts")
+      end
+
+      def click_community_header_button
+        page.click_button(
+          I18n.t("js.sidebar.sections.community.header_action_title"),
+          class: "sidebar-section-header-button",
+        )
+      end
+
+      def click_everything_link
+        page.click_link(
+          I18n.t("js.sidebar.sections.community.links.everything.content"),
+          class: "sidebar-section-link-everything",
+        )
+      end
+
+      def click_toggle_to_desktop_view_button
+        page.click_button(
+          I18n.t("js.desktop_view"),
+          class: "sidebar-footer-actions-toggle-mobile-view",
+        )
       end
     end
   end
